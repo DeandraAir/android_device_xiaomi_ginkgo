60faa0f7b6d7625de5cf2cb5965ae343472d7c61
DeandraAir <affaantuh407@gmail.com>
2023-11-16 06:19:23 +0700

    dean

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/afterlife_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    afterlife_ginkgo-user \
+    afterlife_ginkgo-userdebug \
+    afterlife_ginkgo-eng
